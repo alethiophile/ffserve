ee6541729fad3134d9d302ce97db520f91ad32f1
Tom Hunt <tomdicksonhunt@gmail.com>
2019-01-12 19:45:25 -0700

    Many feature updates

--- ffserve/__init__.py
+++ ffserve/__init__.py
@@ -1,87 +1,204 @@
 #!/usr/bin/python
 
-import ffmirror.mirror as mirror
-from datetime import datetime
+import ffmirror.metadb as metadb
 import os, argparse
 
-from flask import Flask, g, render_template, url_for, request
-app = Flask('ffserve')
+from itertools import chain
+from operator import attrgetter
 
-if __name__=='__main__':
-    ap = argparse.ArgumentParser(description="Browse an ffmirror archive over the Web")
-    ap.add_argument("-d", "--debug", dest="DEBUG", action="store_true", help="Debug in browser", default=False)
-    ap.add_argument("--page-thres", dest="PAGE_THRES", type=int, help="Minimum story entries per page", default=100)
-    ap.add_argument("-l", "--local", action="store_true", help="Listen only on local interface", default=False)
-    ap.add_argument("-p", "--port", type=int, help="HTTP server port", default=5000)
-    ap.add_argument("FF_DIR", help="Mirror directory")
-    args = ap.parse_args()
+from flask import (Flask, g, render_template, url_for, request, abort,
+                   send_from_directory, redirect)
+app = Flask('ffserve', instance_relative_config=True)
+app.config.from_pyfile('config.cfg')
 
-app.config.from_object(args)
+def template_function(func):
+    app.jinja_env.globals[func.__name__] = func
+    return func
+
+@template_function
+def page_url(pagenum):
+    args = request.view_args.copy()
+    args.update(request.args)
+    args['page'] = pagenum
+    return url_for(request.endpoint, **args)
+
+@template_function
+def find_story(site, sid):
+    ind = g.mirror.get_index(check=False)
+    for i in chain(*[i.stories for i in ind.values()]):
+        if i['site'] == site and i['id'] == sid:
+            return i
+    return None
+
+@app.template_filter()
+def format_number(n):
+    return "{:,}".format(n)
+
+@app.template_filter()
+def format_date(n):
+    return n.date().isoformat()
+
+@app.template_filter()
+def sort_by(slist, order):
+    nl = slist.copy()
+    nl.sort(key=attrgetter('title'))  # secondary sort for those that matter
+    nl.sort(key=attrgetter(order))
+    if order in ['updated', 'words']:
+        nl.reverse()
+    return nl
+
+@app.template_filter()
+def stories_present(slist):
+    return [i for i in slist if i.download_fn is not None]
 
 @app.before_request
 def req_setup():
-    g.mirror = mirror.FFMirror(app.config['FF_DIR'])
-    g.url_for = url_for
-    def format_number(n):
-        return "{:,}".format(n)
-    g.format_number = format_number
-    def format_date(n):
-        return datetime.fromtimestamp(int(n)).date().isoformat()
-    g.format_date = format_date
-    def in_page(author, pages):
-        for n, i in enumerate(pages):
-            if author in i:
-                return n
-    g.in_page = in_page
+    g.mirror = metadb.DBMirror(app.config['FF_DIR'], debug=True)
+    g.mirror.connect()
+    app.config['FAV_DIR'] = os.path.join(app.config['FF_DIR'], '.favs')
 
-def make_pages(ind):
-    """This function takes an index (type {author: [story]}) and returns a list of
-    pages. A page is a list of authors who will be on that page."""
+def sort_query(aq):
+    al = []
+    for auth, story in aq.all():
+        if len(al) == 0 or al[-1] is not auth:
+            al.append(auth)
+            auth.query_stories = []
+        auth.query_stories.append(story)
+    return al
+
+def query_pages(alist):
+    """A generator that returns pages (type [metadb.Author]) for the given
+    query.
+
+    """
+    rv = []
+    cp = 0
     page_thres = app.config['PAGE_THRES']
-    il = sorted(iter(ind.keys()), key= lambda x: x.lower())
-    cl = 0
-    ol = []
-    lc = 0
-    for n,i in enumerate(il):
-        cl += len(ind[i])
-        if cl >= page_thres:
-            cl = 0
-            ol.append((lc, n+1))
-            lc = n+1
-    if len(ol) == 0 or ol[-1][1] != len(il):
-        ol.append((lc, len(il)))
-    return [il.__getitem__(slice(*i)) for i in ol]
+    for i in alist:
+        rv.append(i)
+        cp += len(i.query_stories)
+        if cp >= page_thres:
+            yield rv
+            rv = []
+            cp = 0
+    yield rv
+
+@template_function
+def get_page(ind, alist):
+    """This function returns a page (list of metadb.Author) given its index. Second
+    return value is a boolean stating whether requested page is the last one.
+
+    """
+    if ind == -1:
+        return alist, True
+    pl = list(query_pages(alist))
+    return pl[ind], ind >= len(pl) - 1
+
+@template_function
+def find_author(ao, alist):
+    for n, i in enumerate(query_pages(alist)):
+        if ao in i:
+            return n
+    return None
 
 @app.route('/')
 def index():
-    ind = g.mirror.get_index()
     pagenum = int(request.args.get('page', 0))
-    pages = make_pages(ind)
-    if pagenum == -1:
-        pages = [ [i for sl in pages for i in sl] ]
-        pagenum = 0
-    return render_template('main_index.html', ind=ind, pages=pages, pagenum=pagenum)
+    query = (g.mirror.ds.query(metadb.Author, metadb.Story)
+             .options(metadb.joinedload(metadb.Story.tags))
+             .filter(metadb.Story.author_id == metadb.Author.id)
+             .filter(metadb.Author.in_mirror == True)  # noqa: E712
+             .order_by(metadb.func.lower(metadb.Author.name)))
+    return render_template('main_index.html', pagenum=pagenum,
+                           alist=sort_query(query), tag=None)
+
+@app.route('/list/<author>')
+def to_author(author):
+    try:
+        ao = g.mirror.ds.query(metadb.Author).filter_by(id=author).one()
+    except metadb.exc.NoResultFound:
+        abort(404)
+    query = (g.mirror.ds.query(metadb.Author, metadb.Story)
+             .filter(metadb.Story.author_id == metadb.Author.id)
+             .filter(metadb.Author.in_mirror == True)  # noqa: E712
+             .order_by(metadb.text("lower(name)")))
+    page = find_author(ao, sort_query(query))
+    if page is not None:
+        return redirect(url_for('index', page=page) + '#' + author)
+    abort(404)
 
 @app.route('/story/<path:filepath>')
 def story(filepath):
-    if '/../' in filepath or filepath.startswith('../'):
-        abort(403)
-    fn = os.path.join(app.config['FF_DIR'], filepath)
-    with open(fn, 'r') as infile:
-        return infile.read()
+    if not filepath.endswith('.html'):
+        abort(404)
+    return send_from_directory(app.config['FF_DIR'], filepath,
+                               mimetype='text/html')
 
 @app.route('/tag/<path:tagname>')
 def tag(tagname):
-    ind = g.mirror.get_index()
     pagenum = int(request.args.get('page', 0))
-    ind = { k: [i for i in ind[k] if tagname in i['tags']] for k in ind.keys() }
-    ind = { k: ind[k] for k in ind.keys() if len(ind[k]) != 0 }
-    pages = make_pages(ind)
-    if pagenum == -1:
-        pages = [ [i for sl in pages for i in sl] ]
-        pagenum = 0
-    return render_template('main_index.html', ind=ind, pages=pages, pagenum=pagenum, tag=tagname)
+    to = g.mirror.ds.query(metadb.Tag).filter_by(name=tagname).one()
+    query = (g.mirror.ds.query(metadb.Author, metadb.Story)
+             .options(metadb.joinedload(metadb.Story.tags))
+             .filter(metadb.Story.tags.contains(to))
+             .filter(metadb.Story.author_id == metadb.Author.id)
+             .filter(metadb.Author.in_mirror == True)  # noqa: E712
+             .order_by(metadb.func.lower(metadb.Author.name)))
+    return render_template('main_index.html', pagenum=pagenum,
+                           alist=sort_query(query), tag=tagname)
 
-if __name__=='__main__':
+@app.route('/favs/<author>')
+def favs(author):
+    order = request.args.get('sort', 'updated')
+    try:
+        ao = g.mirror.ds.query(metadb.Author).filter_by(id=author).one()
+    except metadb.exc.NoResultFound:
+        abort(404)
+    g.mirror.sync_author(ao)
+    ao = (g.mirror.ds.query(metadb.Author)
+          .options(  # metadb.joinedload(metadb.Author.fav_stories)
+                     # .joinedload('author'),
+              metadb.joinedload(metadb.Author.fav_stories)
+              .joinedload('tags'),
+              metadb.joinedload(metadb.Author.stories_written)
+              .joinedload('tags'))
+          .filter_by(id=author).one())
+    return render_template('favorites.html', ao=ao, order=order)
+
+@app.route('/favorite/<sid>')
+def favorite(sid):
+    try:
+        so = g.mirror.ds.query(metadb.Story).filter_by(id=sid).one()
+    except metadb.exc.NoResultFound:
+        abort(404)
+    sfn = os.path.join('.favs', so.unique_filename())
+    if so.download_time is None or so.updated > so.download_time:
+        g.mirror.story_to_archive(so, rfn=sfn, silent=True)
+    return redirect(url_for('story', filepath=sfn))
+
+if __name__ == '__main__':
+    import autoport
+    ap = argparse.ArgumentParser(description="Browse an ffmirror archive over the Web")
+    ap.add_argument("-d", "--debug", dest="DEBUG", action="store_true",
+                    help="Debug in browser", default=False)
+    ap.add_argument("--page-thres", dest="PAGE_THRES", type=int,
+                    help="Minimum story entries per page", default=100)
+    ap.add_argument("-l", "--local", action="store_true",
+                    help="Listen only on local interface", default=False)
+    ap.add_argument("-p", "--port", help="HTTP server port", default='5000')
+    ap.add_argument("-e", "--escaped", action="store_true",
+                    help="Autoport-escaped directory value", default=False)
+    ap.add_argument("FF_DIR", help="Mirror directory")
+    args = ap.parse_args()
+    if args.escaped:
+        args.FF_DIR = autoport.unescape_path(args.FF_DIR)
+    app.config.from_object(args)
+
+    if args.port == 'auto':
+        args.port = autoport.get_port('ffserve:' +
+                                      autoport.escape_path(args.FF_DIR))
+    else:
+        args.port = int(args.port)
+
     host = '127.0.0.1' if args.local else '0.0.0.0'
     app.run(host=host, port=args.port)
